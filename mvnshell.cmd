feea0b5a22d59db5525e731cbbb77f11109fbcae
Dan Davis <daniel.davis@nih.gov>
2019-04-04 15:06:01 -0400

    Update mvnshell batch process to assume copied dependencies

--- mvnshell.cmd
+++ mvnshell.cmd
@@ -1,7 +1,2 @@
 @echo off
-call mvn dependency:build-classpath ^
-	-DincludeTypes=jar ^
-	-Dmdep.outputFile=target\classpath.txt
-set /p _CP=<target\classpath.txt
-set _CP=%_CP%;target\classes
-jshell --class-path %_CP%
+jshell --class-path target\classes;target\lib\*
