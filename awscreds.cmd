3a4af81e9ace7a9d891c773efd14fbf922cde50e
Dan Davis <daniel.davis@nih.gov>
2019-06-11 12:08:41 -0400

    A windows script that runs getawscreds with a profile and then sources the result

---
+++ awscreds.cmd
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+SET arg_count=0
+FOR %%x in (%*) DO SET /a arg_count += 1
+
+IF %arg_count% == 1 GOTO HAVEARGS
+ECHO Usage: awscreds {profile}
+EXIT /B 1
+
+:HAVEARGS
+SET PROFILE=%1
+SET TEMPFILE=%TEMP%\awscreds-out.cmd
+CALL getawscreds -p %PROFILE% -o %TEMPFILE%
+
+IF EXIST %TEMPFILE% GOTO HAVEFILE
+ECHO getawscreds did not produce an output file
+EXIT /B 1
+
+:HAVEFILE
+ECHO Invoking %TEMPFILE%
+%TEMPFILE%
