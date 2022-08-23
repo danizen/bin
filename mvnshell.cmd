b9742264c261ee21ce369ce0b86a6fb0d723ce7b
Dan Davis <dan@danizen.net>
2022-08-23 13:50:00 -0400

    Updating the mvnshell commands.

--- mvnshell.cmd
+++ mvnshell.cmd
@@ -8,14 +8,14 @@ CALL mvn dependency:copy-dependencies
 
 :RUNSHELL
 IF %NARGS% NEQ 0 (
-	jshell --class-path target\classes;target\test-classes;target\lib\* %*
+	jshell --class-path target\classes;target\test-classes;target\dependency\* %*
 	GOTO END
 )
 
 IF EXIST "setup.jsh" (
-	jshell --class-path target\classes;target\test-classes;target\lib\* -start setup.jsh
+	jshell --class-path target\classes;target\test-classes;target\dependency\* -start setup.jsh
 	GOTO END
 )
 
-jshell --class-path target\classes;target\test-classes;target\lib\*
+jshell --class-path target\classes;target\test-classes;target\dependency\*
 :END
--- mvnshell.sh
+++ mvnshell.sh
@@ -5,7 +5,7 @@ if [[ -d target && ! -d target/lib ]]; then
 fi
 
 if [[ $# -eq 0 && -f "setup.jsh" ]]; then
-    jshell --class-path target/dependency:target/test-classes:target/classes -start setup.jsh
+    jshell --class-path target/dependency/*:target/test-classes:target/classes -start setup.jsh
 else
-    jshell --class-path target/dependency:target/test-classes:target/classes $*
+    jshell --class-path target/dependency/*:target/test-classes:target/classes $*
 fi
